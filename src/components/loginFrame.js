a959dc920b2927355a7b3807af81fb1221d3ef8e
wnttmk <93745499@qq.com>
2020-03-18 17:55:52 +0800

    '备注了一下'

--- src/components/loginFrame.js
+++ src/components/loginFrame.js
@@ -14,7 +14,7 @@ export default class loginFrame extends Component{
                 <br></br>
                 <Link to={`${this.props.match.url}/r`}>注册</Link> | <Link to={`${this.props.match.url}`}>登录</Link>
                    
-                        
+                        {/* 这里不需要再加Router了,因为我们是被包含的.在router.js中已经加了该标签 */}
                         <Route path={`${this.props.match.url}/r`}  component={register}></Route>
                         <Route path={this.props.match.url} exact component={loginform}></Route>
                          
